93142dacb44bf2e0ce011061c21ba8912c441660
VinceAAG <vincent.johnson@echelongov.com>
2023-10-05 16:35:42 -0700

    Initial commit

---
+++ Pages/role_details.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CAR2_UI.Pages
+{
+	public class role_detailsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
